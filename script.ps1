9bff1db45b100d3e0011cf49d55a621c4f47d784
VL-IDB <VL-IDB@iadb.org>
2017-12-20 16:23:18 -0500

    change-prod

--- script.ps1
+++ script.ps1
@@ -5,7 +5,7 @@ $proxy = [System.Net.CredentialCache]::DefaultCredentials
 Add-AzureRMAccount
 #Get-AzureRmSubscription
 #Set-AzureRmContext -SubscriptionName "Enterprise Dev/Test"
-$resourcegroup = "rg-p-pw-paloalto01"
+$resourcegroup = "rg-p-fw-paloalto"
 New-AzureRmResourceGroup -Name $resourcegroup -Force -Location "East US2"
 $rmtem = "C:\dev\azure-applicationgateway-master\azure-applicationgateway-master\azuredeploy.json"
 $rmpar = "C:\dev\azure-applicationgateway-master\azure-applicationgateway-master\azureDeploy.parameters.json"
